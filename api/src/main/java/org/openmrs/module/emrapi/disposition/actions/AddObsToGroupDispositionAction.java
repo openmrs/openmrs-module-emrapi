c12e67b2c298210c2b5f8ae2bf6d9f5577cf568b
Mário <mario.s.s.areias@gmail.com>
2013-05-16 15:47:38 -0300

    #500 creating new action for Transfer out of hospital

---
+++ api/src/main/java/org/openmrs/module/emrapi/disposition/actions/AddObsToGroupDispositionAction.java
@@ -0,0 +1,41 @@
+package org.openmrs.module.emrapi.disposition.actions;
+
+import org.openmrs.Concept;
+import org.openmrs.Obs;
+import org.openmrs.module.emrapi.concept.EmrConceptService;
+import org.openmrs.module.emrapi.encounter.EncounterDomainWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component("addObsToGroupDispositionAction")
+public class AddObsToGroupDispositionAction implements DispositionAction {
+    private static final String CONCEPT = "concept";
+    private static final String VALUE = "valueCoded";
+
+    @Autowired
+    private EmrConceptService emrConceptService;
+
+    @Override
+    public void action(EncounterDomainWrapper encounterDomainWrapper, Obs dispositionObsGroupBeingCreated, Map<String, String[]> requestParameters) {
+        String concept = DispositionActionUtils.getSingleRequiredParameter(requestParameters, CONCEPT);
+        String value = DispositionActionUtils.getSingleRequiredParameter(requestParameters, VALUE);
+
+        Concept question = emrConceptService.getConcept(concept);
+        Concept answer = emrConceptService.getConcept(value);
+
+        Obs obs = createObs(question, answer);
+
+        encounterDomainWrapper.getEncounter().addObs(obs);
+
+
+    }
+
+    private Obs createObs(Concept question, Concept answer) {
+        Obs obs = new Obs();
+        obs.setConcept(question);
+        obs.setValueCoded(answer);
+        return obs;
+    }
+}
