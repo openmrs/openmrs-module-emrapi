e72acc6589784d4cd00af4dfcf3e6b25a3ec93e0
djazayeri <jazayeri@alum.mit.edu>
2013-07-23 16:21:06 -0700

    Mirebalais #773 - Integration test for new/old concepts in mds packages

--- api/src/main/java/org/openmrs/module/emrapi/utils/MetadataUtil.java
+++ api/src/main/java/org/openmrs/module/emrapi/utils/MetadataUtil.java
@@ -13,13 +13,16 @@
  */
 package org.openmrs.module.emrapi.utils;
 
+import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Auditable;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.emrapi.metadata.MetadataPackageConfig;
 import org.openmrs.module.emrapi.metadata.MetadataPackagesConfig;
 import org.openmrs.module.metadatasharing.ImportConfig;
+import org.openmrs.module.metadatasharing.ImportedItem;
 import org.openmrs.module.metadatasharing.ImportedPackage;
 import org.openmrs.module.metadatasharing.MetadataSharing;
 import org.openmrs.module.metadatasharing.api.MetadataSharingService;
@@ -27,6 +30,13 @@ import org.openmrs.module.metadatasharing.wrapper.PackageImporter;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -111,4 +121,129 @@ public class MetadataUtil {
 			return false;
 		}
 	}
+
+    /**
+     * If multiple MDS packages contain different versions of the same item, then loading them is order-dependent, which
+     * is bad.
+     * Any OpenMRS distribution that uses the #installMetadataPackageIfNecessary functionality in this class should
+     * have a one-line unit test that calls this method, basically like:
+     * <pre>
+     * public class InconsistantMetadataTest extends BaseModuleContextSensitiveTest {
+     *     @Test
+     *     public void testThatThereAreNoMdsPackagesWithInconsistentVersionsOfTheSameItem() throws Exception {
+     *         MetadataUtil.verifyNoMdsPackagesWithInconsistentVersionsOfTheSameItem(getClass().getClassLoader());
+     *     }
+     * }
+     * </pre>
+     *
+     * @param classLoader
+     * @throws Exception
+     * @throws IllegalStateException if different versions of the same metadata item are contained in two packages
+     */
+    public static void verifyNoMdsPackagesWithInconsistentVersionsOfTheSameItem(ClassLoader classLoader) throws Exception, IllegalStateException {
+        ItemToDateMap itemToDateMap = new ItemToDateMap();
+
+        MetadataPackagesConfig allConfigs = MetadataUtil.getMetadataPackagesForModule(classLoader);
+        for (MetadataPackageConfig config : allConfigs.getPackages()) {
+            String filenameBase = config.getFilenameBase();
+            String filename = filenameBase + "-" + config.getVersion().toString() + ".zip";
+            log.debug("Inspecting " + filename);
+            System.out.println("Inspecting " + filename);
+
+            PackageImporter metadataImporter = MetadataSharing.getInstance().newPackageImporter();
+            metadataImporter.setImportConfig(ImportConfig.valueOf(config.getImportMode()));
+            metadataImporter.loadSerializedPackageStream(classLoader.getResourceAsStream(filename));
+            for (int i = 0; i < metadataImporter.getPartsCount(); ++i) {
+                Collection<ImportedItem> items = metadataImporter.getImportedItems(i);
+                for (ImportedItem item : items) {
+                    itemToDateMap.addItem(item, filenameBase);
+                    for (ImportedItem related : item.getRelatedItems()) {
+                        itemToDateMap.addItem(related, filenameBase);
+                    }
+                }
+            }
+
+            log.debug("Finished. Running total number of distinct items: " + itemToDateMap.size());
+            System.out.println("Finished. Running total number of distinct items: " + itemToDateMap.size());
+        }
+
+        if (log.isInfoEnabled()) {
+            Map<String, Set<String>> repeated = itemToDateMap.repeatedItems();
+            if (log.isDebugEnabled()) {
+                log.debug("Items that occur in multiple MDS packages:");
+                for (Map.Entry<String, Set<String>> e : repeated.entrySet()) {
+                    log.debug(e.getKey() + " -> " + e.getValue());
+                }
+            }
+            log.info("Number of distinct items in multiple packages: " + repeated.size());
+            log.info("Total number of distinct items: " + itemToDateMap.size());
+        }
+    }
+
+    static class ItemToDateMap {
+
+        // classname + uuid to last date modified
+        Map<String, Date> lastModifiedMap = new HashMap<String, Date>();
+        Map<String, Set<String>> itemToPackages = new HashMap<String, Set<String>>();
+
+        public void addItem(ImportedItem item, String filename) {
+            String key = getKey(item);
+            Date lastModified = getLastModified(item.getIncoming());
+
+            Set<String> belongsToPackages = itemToPackages.get(key);
+            if (belongsToPackages == null) {
+                belongsToPackages = new TreeSet<String>();
+                itemToPackages.put(key, belongsToPackages);
+            }
+            belongsToPackages.add(filename);
+
+            Date existing = lastModifiedMap.get(key);
+            if (existing == null) {
+                lastModifiedMap.put(key, lastModified);
+            }
+            else {
+                if (!existing.equals(lastModified)) {
+                    throw new IllegalStateException("Found inconsistent versions of " + key + " in " + belongsToPackages);
+                }
+            }
+        }
+
+        private Date getLastModified(Object object) {
+            if (object instanceof Auditable) {
+                Date dateChanged = ((Auditable) object).getDateChanged();
+                if (dateChanged != null) {
+                    return dateChanged;
+                } else {
+                    return ((Auditable) object).getDateCreated();
+                }
+            }
+            else {
+                throw new IllegalArgumentException("object must be Auditable");
+            }
+        }
+
+        private String getKey(ImportedItem item) {
+            try {
+                return item.getIncomingClassSimpleName() + ":" + PropertyUtils.getProperty(item.getIncoming(), "uuid");
+            } catch (Exception e) {
+                throw new RuntimeException("Couldn't get UUID from " + item.getIncoming());
+            }
+        }
+
+        public int size() {
+            return lastModifiedMap.size();
+        }
+
+        public Map<String, Set<String>> repeatedItems() {
+            Map<String, Set<String>> repeatedItems = new TreeMap<String, Set<String>>();
+            for (Map.Entry<String, Set<String>> candidate : itemToPackages.entrySet()) {
+                if (candidate.getValue().size() > 1) {
+                    repeatedItems.put(candidate.getKey(), candidate.getValue());
+                }
+            }
+            return repeatedItems;
+        }
+
+    }
+
 }
