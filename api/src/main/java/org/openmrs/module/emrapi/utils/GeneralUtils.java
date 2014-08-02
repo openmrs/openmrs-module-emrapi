37afcf0d0c18d5f315bb204668966dbd0ddd57f1
mihirk <mihir.khatwani@gmail.com>
2014-08-02 16:06:19 +0530

    Mihir | removing the java.util.* and adding java.util.Date

--- api/src/main/java/org/openmrs/module/emrapi/utils/GeneralUtils.java
+++ api/src/main/java/org/openmrs/module/emrapi/utils/GeneralUtils.java
@@ -31,7 +31,11 @@ import org.openmrs.util.OpenmrsConstants;
 import org.openmrs.util.OpenmrsUtil;
 
 import java.lang.reflect.Method;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Date;
 
 /**
  *
