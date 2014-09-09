e5b629a69615dfa40932199b7fd8b3fd26de6d28
Paweł Muchowski <pmuchowski@soldevelo.com>
2014-09-09 14:30:54 +0200

    EA-29: Add search handler for last viewed patients

--- api-1.9/src/main/java/org/openmrs/module/emrapi/encounter/DrugOrderMapper_1_9.java
+++ api-1.9/src/main/java/org/openmrs/module/emrapi/encounter/DrugOrderMapper_1_9.java
@@ -14,12 +14,14 @@
 package org.openmrs.module.emrapi.encounter;
 
 import org.openmrs.DrugOrder;
+import org.openmrs.annotation.OpenmrsProfile;
 import org.openmrs.api.ConceptService;
 import org.openmrs.module.emrapi.encounter.domain.EncounterTransaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component (value = "drugOrderMapper")
+@OpenmrsProfile(openmrsVersion = "1.9")
 public class DrugOrderMapper_1_9 extends DrugOrderMapperBaseImpl implements DrugOrderMapper {
     private ConceptService conceptService;
 
---
+++ omod/src/main/java/org/openmrs/module/emrapi/web/rest/searchhandler/LastViewedPatientsSearchHandler.java
@@ -0,0 +1,61 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.emrapi.web.rest.searchhandler;
+
+import org.openmrs.Patient;
+import org.openmrs.User;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.resource.api.PageableResult;
+import org.openmrs.module.webservices.rest.web.resource.api.SearchConfig;
+import org.openmrs.module.webservices.rest.web.resource.api.SearchHandler;
+import org.openmrs.module.webservices.rest.web.resource.api.SearchQuery;
+import org.openmrs.module.webservices.rest.web.resource.impl.NeedsPaging;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
+import org.springframework.stereotype.Component;
+import org.openmrs.module.emrapi.utils.GeneralUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@Component
+public class LastViewedPatientsSearchHandler implements SearchHandler {
+
+    private final SearchConfig searchConfig = new SearchConfig("default", RestConstants.VERSION_1 + "/patient", Arrays.asList(
+            "1.8.*", "1.9.*", "1.10.*"), new SearchQuery.Builder("Allows you to find last viewed patients").withRequiredParameters("lastviewed")
+            .build());
+
+    /**
+     * @see org.openmrs.module.webservices.rest.web.resource.api.SearchHandler#getSearchConfig()
+     */
+    @Override
+    public SearchConfig getSearchConfig() {
+        return searchConfig;
+    }
+
+    /**
+     * @see org.openmrs.module.webservices.rest.web.resource.api.SearchHandler#search(org.openmrs.module.webservices.rest.web.RequestContext)
+     */
+    @Override
+    public PageableResult search(RequestContext context) throws ResponseException {
+
+        User user = Context.getAuthenticatedUser();
+
+        List<Patient> lastViewedPatients = GeneralUtils.getLastViewedPatients(user);
+
+        return new NeedsPaging<Patient>(lastViewedPatients, context);
+    }
+}
