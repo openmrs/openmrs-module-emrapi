485ee143353d612ca75daf771691f2ee4b1bf80d
mogoodrich <mgoodrich@pih.org>
2015-05-22 17:18:11 -0400

    EA-66:Close Stale Visits task should not fail if Dispositions not configured

--- api/src/main/java/org/openmrs/module/emrapi/adt/AdtServiceImpl.java
+++ api/src/main/java/org/openmrs/module/emrapi/adt/AdtServiceImpl.java
@@ -39,6 +39,7 @@ import org.openmrs.api.impl.BaseOpenmrsService;
 import org.openmrs.module.emrapi.EmrApiConstants;
 import org.openmrs.module.emrapi.EmrApiProperties;
 import org.openmrs.module.emrapi.adt.exception.ExistingVisitDuringTimePeriodException;
+import org.openmrs.module.emrapi.descriptor.MissingConceptException;
 import org.openmrs.module.emrapi.disposition.Disposition;
 import org.openmrs.module.emrapi.domainwrapper.DomainWrapperFactory;
 import org.openmrs.module.emrapi.merge.PatientMergeAction;
@@ -139,15 +140,20 @@ public class AdtServiceImpl extends BaseOpenmrsService implements AdtService {
             return false;  // already closed
         }
 
-        VisitDomainWrapper visitDomainWrapper = domainWrapperFactory.newVisitDomainWrapper(visit);
+        try {
+            VisitDomainWrapper visitDomainWrapper = domainWrapperFactory.newVisitDomainWrapper(visit);
 
-        if (visitDomainWrapper.isAdmitted() || visitDomainWrapper.isAwaitingAdmission()) {
-            return false;  // don't close the visit if patient is admitted or waiting admission
+            if (visitDomainWrapper.isAdmitted() || visitDomainWrapper.isAwaitingAdmission()) {
+                return false;  // don't close the visit if patient is admitted or waiting admission
+            }
+
+            Disposition mostRecentDisposition = visitDomainWrapper.getMostRecentDisposition();
+            if (mostRecentDisposition != null && mostRecentDisposition.getKeepsVisitOpen() != null && mostRecentDisposition.getKeepsVisitOpen()) {
+                return false; // don't close the visit if the most recent disposition is one that keeps visit opens
+            }
         }
-
-        Disposition mostRecentDisposition = visitDomainWrapper.getMostRecentDisposition();
-        if (mostRecentDisposition != null && mostRecentDisposition.getKeepsVisitOpen() != null && mostRecentDisposition.getKeepsVisitOpen()) {
-            return false; // don't close the visit if the most recent disposition is one that keeps visit opens
+        catch (MissingConceptException ex) {
+            // we don't want to fail if dispositions aren't configured
         }
 
         Date now = new Date();
