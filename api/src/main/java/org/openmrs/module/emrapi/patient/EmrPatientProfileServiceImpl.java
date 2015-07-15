9627a9ecb2a89277483ab672823ee88dc20d20d8
Hemanth S.R <hemanthfeb25@gmail.com>
2015-07-15 17:06:16 +0530

    Revert "Revert "EA-71 | Able to CRUD relationships for patient using PatientProfileResource""
    This reverts commit 6403d907880b4ad32fa6712840c3cdc527b019a9.

--- api/src/main/java/org/openmrs/module/emrapi/patient/EmrPatientProfileServiceImpl.java
+++ api/src/main/java/org/openmrs/module/emrapi/patient/EmrPatientProfileServiceImpl.java
@@ -1,30 +1,63 @@
 package org.openmrs.module.emrapi.patient;
 
 import org.openmrs.Patient;
+import org.openmrs.Person;
+import org.openmrs.Relationship;
 import org.openmrs.api.PatientService;
+import org.openmrs.api.PersonService;
 import org.openmrs.module.emrapi.person.image.EmrPersonImageService;
 import org.openmrs.module.emrapi.person.image.PersonImage;
+
+import java.util.List;
 
 public class EmrPatientProfileServiceImpl implements EmrPatientProfileService {
 
     private PatientService patientService;
+    private PersonService personService;
     private EmrPersonImageService emrPersonImageService;
     
     @Override
     public PatientProfile save(PatientProfile patientProfile) {
         Patient patient = patientService.savePatient(patientProfile.getPatient());
+
+        saveRelationships(patientProfile.getRelationships());
+
         patientProfile.setPatient(patient);
 
         PersonImage personImage = new PersonImage();
         personImage.setPerson(patient);
         personImage.setBase64EncodedImage(patientProfile.getImage());
-        
+
         emrPersonImageService.savePersonImage(personImage);
         return patientProfile;
     }
 
+    @Override
+    public PatientProfile get(String patientUuid) {
+        PatientProfile delegate = new PatientProfile();
+
+        Patient patient = patientService.getPatientByUuid(patientUuid);
+        delegate.setPatient(patient);
+
+        Person person = personService.getPerson(patient.getPersonId());
+        List<Relationship> relationships = personService.getRelationshipsByPerson(person);
+        delegate.setRelationships(relationships);
+
+        return delegate;
+    }
+
+    private void saveRelationships(List<Relationship> relationships) {
+        for(Relationship relationship: relationships) {
+            personService.saveRelationship(relationship);
+        }
+    }
+
     public void setPatientService(PatientService patientService) {
         this.patientService = patientService;
+    }
+
+    public void setPersonService(PersonService personService) {
+        this.personService = personService;
     }
 
     public void setEmrPersonImageService(EmrPersonImageService emrPersonImageService) {
--- omod/src/main/java/org/openmrs/module/emrapi/rest/resource/PatientProfileResource.java
+++ omod/src/main/java/org/openmrs/module/emrapi/rest/resource/PatientProfileResource.java
@@ -1,7 +1,13 @@
 package org.openmrs.module.emrapi.rest.resource;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.openmrs.Patient;
+import org.openmrs.Person;
+import org.openmrs.Relationship;
+import org.openmrs.RelationshipType;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.emrapi.encounter.DateMapper;
 import org.openmrs.module.emrapi.patient.PatientProfile;
 import org.openmrs.module.emrapi.rest.EmrModuleContext;
 import org.openmrs.module.webservices.rest.SimpleObject;
@@ -17,7 +23,11 @@ import org.openmrs.module.webservices.rest.web.response.ConversionException;
 import org.openmrs.module.webservices.rest.web.response.ResourceDoesNotSupportOperationException;
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
 import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.PatientResource1_8;
+import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.PersonResource1_8;
+import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.RelationShipTypeResource1_8;
+import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_8.RelationshipResource1_8;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -35,6 +45,7 @@ public class PatientProfileResource extends DelegatingCrudResource<PatientProfil
         DelegatingResourceDescription description = new DelegatingResourceDescription();
         description.addProperty("patient", Representation.FULL);
         description.addProperty("image");
+        description.addProperty("relationships", Representation.DEFAULT);
         return description;
     }
 
@@ -43,6 +54,7 @@ public class PatientProfileResource extends DelegatingCrudResource<PatientProfil
         DelegatingResourceDescription description = new DelegatingResourceDescription();
         description.addProperty("patient", Representation.DEFAULT);
         description.addProperty("image", Representation.DEFAULT);
+        description.addProperty("relationships", Representation.DEFAULT);
         return description;
     }
 
@@ -51,6 +63,7 @@ public class PatientProfileResource extends DelegatingCrudResource<PatientProfil
         DelegatingResourceDescription description = new DelegatingResourceDescription();
         description.addProperty("patient", Representation.DEFAULT);
         description.addProperty("image", Representation.DEFAULT);
+        description.addProperty("relationships", Representation.DEFAULT);
         return description;
     }
 
@@ -66,30 +79,127 @@ public class PatientProfileResource extends DelegatingCrudResource<PatientProfil
         delegate.setPatient(patientResource1_9.getPatient(new SimpleObject() {{
             putAll((Map<String, Object>) patientProperty);
         }}));
+
         propertiesToCreate.removeProperty("patient");
+        delegate.setRelationships(getRelationships(propertiesToCreate, delegate.getPatient()));
+        propertiesToCreate.removeProperty("relationships");
 
         setConvertedProperties(delegate, propertiesToCreate, getCreatableProperties(), true);
         delegate = save(delegate);
         return ConversionUtil.convertToRepresentation(delegate, Representation.FULL);
     }
 
+    private List<Relationship> getRelationships(SimpleObject propertiesToCreate, Person currentPerson) {
+        Object relationshipsList = propertiesToCreate.get("relationships");
+        List<Relationship> relationships = new ArrayList<Relationship>();
+        List<Map<String, Object>> relationshipProperties = (List<Map<String, Object>>) relationshipsList;
+        for (final Map<String, Object> relationshipProperty : relationshipProperties) {
+            String uuid = getValueFromMap(relationshipProperty, "uuid");
+            Relationship relationship;
+            if (StringUtils.isBlank(uuid)) {
+                relationship = createRelationship(currentPerson, relationshipProperty);
+            } else {
+                relationship = updateRelationship(relationshipProperty);
+            }
+            relationships.add(relationship);
+        }
+        return relationships;
+    }
+
+    private Relationship updateRelationship(final Map<String, Object> relationshipJson) {
+        String relationshipUuid = getValueFromMap(relationshipJson, "uuid");
+
+        if (StringUtils.isBlank(relationshipUuid)) {
+            throw new ConversionException("The relationshipUuid is not present");
+        }
+
+        RelationshipResource1_8 relationshipResource = (RelationshipResource1_8) Context.getService(RestService.class).getResourceBySupportedClass(Relationship.class);
+        Relationship relationship = relationshipResource.getByUniqueId(relationshipUuid);
+
+        if (null == relationship) {
+            throw new ConversionException("Invalid relationship for relationshipUuid " + relationshipUuid);
+        }
+
+        relationshipResource.setConvertedProperties(relationship, relationshipJson, relationshipResource.getUpdatableProperties(), true);
+
+        RelationshipType updatedRelationshipType = getRelationshipType((Map<String, Object>) relationshipJson.get("relationshipType"));
+        relationship.setRelationshipType(updatedRelationshipType);
+
+        return relationship;
+    }
+
+    private Relationship createRelationship(Person currentPerson, Map<String, Object> relationshipJson) {
+        Relationship relationship = new Relationship(currentPerson,
+                getPerson((Map<String, Object>) relationshipJson.get("personB")),
+                getRelationshipType((Map<String, Object>) relationshipJson.get("relationshipType")));
+        relationship.setEndDate(new DateMapper().toDate(getValueFromMap(relationshipJson, "endDate")));
+
+        return relationship;
+    }
+
+    private String getValueFromMap(Map<String, Object> jsonMap, String key) {
+        Object value = jsonMap.get(key);
+        return ObjectUtils.toString(value);
+    }
+
+    private Person getPerson(Map<String, Object> personJson) {
+        String personUuid = getValueFromMap(personJson, "uuid");
+
+        if (StringUtils.isBlank(personUuid)) {
+            throw new ConversionException("The personUuid is not present.");
+        }
+
+        return getPersonFromUuid(personUuid);
+    }
+
+    private Person getPersonFromUuid(String personUuid) {
+        PersonResource1_8 personResource = (PersonResource1_8) Context.getService(RestService.class).getResourceBySupportedClass(Person.class);
+        Person person = personResource.getByUniqueId(personUuid);
+
+        if (person == null) {
+            throw new ConversionException("The person does not exist.");
+        }
+        return person;
+    }
+
+    private RelationshipType getRelationshipType(Map<String, Object> relationshipTypeJson) {
+
+        String relationshipTypeUuid = getValueFromMap(relationshipTypeJson, "uuid");
+
+        if (StringUtils.isBlank(relationshipTypeUuid)) {
+            throw new ConversionException("The relationshipTypeUuid is not present");
+        }
+
+        RelationShipTypeResource1_8 relationshipResource = (RelationShipTypeResource1_8) Context.getService(RestService.class).getResourceBySupportedClass(RelationshipType.class);
+        RelationshipType relationshipType = relationshipResource.getByUniqueId(relationshipTypeUuid);
+
+        if (relationshipType == null) {
+            throw new ConversionException("The relationship type does not exist.");
+        }
+
+        return relationshipType;
+    }
+
     @Override
     public Object update(String uuid, SimpleObject propertiesToUpdate, RequestContext context) throws ResponseException {
+
         if (propertiesToUpdate.get("patient") == null || !(propertiesToUpdate.get("patient") instanceof Map)) {
             throw new ConversionException("The patient property is missing");
         }
 
         PatientProfile delegate = new PatientProfile();
+
         PatientResource1_8 patientResource1_9 = (PatientResource1_8) Context.getService(RestService.class).getResourceBySupportedClass(Patient.class);
         Patient patient = patientResource1_9.getPatientForUpdate(uuid, (Map<String, Object>) propertiesToUpdate.get("patient"));
         delegate.setPatient(patient);
 
         propertiesToUpdate.removeProperty("patient");
-        setConvertedProperties(delegate, propertiesToUpdate, getCreatableProperties(), true);
+        setConvertedProperties(delegate, propertiesToUpdate, getUpdatableProperties(), true);
+        delegate.setRelationships(getRelationships(propertiesToUpdate, delegate.getPatient()));
         delegate = save(delegate);
         return ConversionUtil.convertToRepresentation(delegate, Representation.FULL);
     }
-   
+
     @Override
     public PatientProfile newDelegate() {
         return new PatientProfile();
@@ -107,7 +217,13 @@ public class PatientProfileResource extends DelegatingCrudResource<PatientProfil
 
     @Override
     public Object retrieve(String uuid, RequestContext context) throws ResponseException {
-        throw new ResourceDoesNotSupportOperationException("retrieve of patient profile not supported");
+        PatientProfile delegate = EmrModuleContext.getEmrPatientProfileService().get(uuid);
+
+        if (delegate.getPatient() == null) {
+            throw new ConversionException("The patient does not exist.");
+        }
+
+        return ConversionUtil.convertToRepresentation(delegate, Representation.FULL);
     }
 
     @Override
