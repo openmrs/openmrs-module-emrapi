f2474cfdb0c7dbe4060a94a6b5eb0263ea99ba93
Preethi <preethis@thoughtworks.com>
2015-07-29 18:42:12 +0530

    EA-76-test-fix : Fixing the date mapper test with correct date format

--- api/src/test/java/org/openmrs/module/emrapi/encounter/DateMapperTest.java
+++ api/src/test/java/org/openmrs/module/emrapi/encounter/DateMapperTest.java
@@ -6,23 +6,23 @@ import org.junit.Test;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
+import java.util.Locale;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 public class DateMapperTest {
 
     @Test
     public void shouldConvertUTCformatToDate() throws Exception {
         String utcDateString = "2015-07-30T18:30:00.000Z";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyy");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         simpleDateFormat.setTimeZone(TimeZone.getTimeZone("IST"));
 
         Date actualDate = new DateMapper().convertUTCToDate(utcDateString);
 
         assertNotNull(actualDate);
-        assertEquals("31/07/2015", simpleDateFormat.format(actualDate));
+        assertEquals("2015-07-31", simpleDateFormat.format(actualDate));
 
     }
 
