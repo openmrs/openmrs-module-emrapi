cf034a28ef0fe13686eb32b1fb1c9e43bc89b1df
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-10-29 15:29:30 +0100

    EA-83 Tests in other modules depending on emrapi 1.11 and 1.10 fail

--- api/src/main/java/org/openmrs/module/emrapi/encounter/OrderMapper.java
+++ api/src/main/java/org/openmrs/module/emrapi/encounter/OrderMapper.java
@@ -1,14 +1,12 @@
 package org.openmrs.module.emrapi.encounter;
 
+import java.util.List;
+
 import org.openmrs.DrugOrder;
 import org.openmrs.Encounter;
 import org.openmrs.Order;
 import org.openmrs.module.emrapi.encounter.domain.EncounterTransaction;
-import org.springframework.stereotype.Component;
 
-import java.util.List;
-
-@Component
 public interface OrderMapper {
     public List<EncounterTransaction.DrugOrder> mapDrugOrders(Encounter encounter);
 
