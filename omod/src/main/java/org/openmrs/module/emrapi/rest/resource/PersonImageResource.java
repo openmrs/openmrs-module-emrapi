bf1ae5d1529f5d9366ad28d86b0c88d9f845aed6
Preethi <preethi.ms34@gmail.com>
2016-09-23 15:14:27 +0530

    Added 2.0 support for resources

--- omod/src/main/java/org/openmrs/module/emrapi/rest/resource/PersonImageResource.java
+++ omod/src/main/java/org/openmrs/module/emrapi/rest/resource/PersonImageResource.java
@@ -24,7 +24,7 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
-@Resource(name = RestConstants.VERSION_1 + "/personimage", supportedClass = PersonImage.class, supportedOpenmrsVersions = {"1.9.*", "1.10.*", "1.11.*", "1.12.*"})
+@Resource(name = RestConstants.VERSION_1 + "/personimage", supportedClass = PersonImage.class, supportedOpenmrsVersions = {"1.9.*", "1.10.*", "1.11.*", "1.12.*", "2.0.*"})
 public class PersonImageResource extends DelegatingCrudResource<PersonImage> {
 
     @Override
