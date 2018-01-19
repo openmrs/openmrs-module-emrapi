732ce6aa73ddf88457aa4b06c3578cb8164bc182
mgoodrich <mark.goodrich@live.com>
2018-01-19 15:57:13 -0500

    EA-132: AccountDomainWrapper needs to be upgrading to work with Core 2.x

--- api/src/main/java/org/openmrs/module/emrapi/account/AccountDomainWrapper.java
+++ api/src/main/java/org/openmrs/module/emrapi/account/AccountDomainWrapper.java
@@ -19,6 +19,7 @@ import org.openmrs.util.OpenmrsConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import java.lang.reflect.Method;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -337,6 +338,8 @@ public class AccountDomainWrapper implements DomainWrapper {
         }
         user.removeUserProperty(OpenmrsConstants.USER_PROPERTY_LOCKOUT_TIMESTAMP);
         user.removeUserProperty(OpenmrsConstants.USER_PROPERTY_LOGIN_ATTEMPTS);
+
+        // TODO: figure out proper way to make this work in Core 2.x
         userService.createUser(user, null);
     }
 
@@ -348,7 +351,19 @@ public class AccountDomainWrapper implements DomainWrapper {
 
         if (user != null) {
             boolean existingUser = (user.getUserId() != null);
-            userService.createUser(user, password);
+
+            if (!existingUser) {
+                userService.createUser(user, password);
+            }
+            else {
+                // hack to work-around change to service methods for saving users in Core 2.x
+                try {
+                    Method saveUser = UserService.class.getDeclaredMethod("saveUser", User.class);
+                }
+                catch (NoSuchMethodException e) {
+                    userService.createUser(user, password);
+                }
+            }
 
             // the saveUser(user, password) method will *only* set a password for a new user, it won't change an existing one
             if (existingUser && StringUtils.isNotBlank(password) && StringUtils.isNotBlank(confirmPassword)) {
