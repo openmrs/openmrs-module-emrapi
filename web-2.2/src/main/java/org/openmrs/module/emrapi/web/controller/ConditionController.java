371d076fa36c37d85d77e507c3e40d9c89105ce4
Arthur236 <arthur.thungu@gmail.com>
2018-04-24 13:46:02 +0300

    TRUNK-5373: Update the UIs in the reference application to use new condition tables.

--- web-2.2/src/main/java/org/openmrs/module/emrapi/web/controller/ConditionController.java
+++ web-2.2/src/main/java/org/openmrs/module/emrapi/web/controller/ConditionController.java
@@ -3,13 +3,10 @@ package org.openmrs.module.emrapi.web.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openmrs.Condition;
 import org.openmrs.api.ConceptService;
+import org.openmrs.api.ConditionService;
 import org.openmrs.api.PatientService;
-import org.openmrs.module.emrapi.conditionslist.ConditionService;
-import org.openmrs.module.emrapi.conditionslist.contract.Condition;
-import org.openmrs.module.emrapi.conditionslist.contract.ConditionHistory;
-import org.openmrs.module.emrapi.conditionslist.contract.ConditionHistoryMapper;
-import org.openmrs.module.emrapi.conditionslist.contract.ConditionMapper;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,45 +16,61 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * This class specifies data manipulation methods on a Condition.
+ */
 @Controller
 @RequestMapping(value = "/rest/emrapi")
 public class ConditionController extends BaseRestController {
-	
-	ConditionMapper conditionMapper = new ConditionMapper();
-	
-	ConditionHistoryMapper conditionHistoryMapper = new ConditionHistoryMapper(conditionMapper);
-	
+
 	ConditionService conditionService;
-	
+
 	PatientService patientService;
-	
+
 	ConceptService conceptService;
-	
+
+	/**
+	 * Constructor to instantiate the ConditionController.
+	 *
+	 * @param conditionService - the condition service
+	 * @param patientService - the patient service
+	 * @param conceptService - the concept service
+	 */
 	@Autowired
 	public ConditionController(ConditionService conditionService, PatientService patientService,
-	                           ConceptService conceptService) {
+			ConceptService conceptService) {
 		this.conditionService = conditionService;
 		this.patientService = patientService;
 		this.conceptService = conceptService;
 	}
-	
+
+	/**
+	 * Gets a list of active conditions.
+	 *
+	 * @param patientUuid - the uuid of a patient
+	 * @return a list of active conditions
+	 */
 	@RequestMapping(method = RequestMethod.GET, value = "/conditionhistory")
 	@ResponseBody
-	public List<ConditionHistory> getConditionHistory(@RequestParam("patientUuid") String patientUuid) {
-		List<org.openmrs.module.emrapi.conditionslist.ConditionHistory> conditionHistory = conditionService.getConditionHistory(
-				patientService.getPatientByUuid(patientUuid));
-		
-		return conditionHistoryMapper.map(conditionHistory);
+	public List<Condition> getConditionHistory(@RequestParam("patientUuid") String patientUuid) {
+
+		return conditionService.getActiveConditions(patientService.getPatientByUuid(patientUuid));
 	}
-	
+
+	/**
+	 * Saves a condition.
+	 *
+	 * @param conditions - a list of conditions to be saved
+	 */
 	@RequestMapping(method = RequestMethod.POST, value = "/condition")
 	@ResponseBody
 	public List<Condition> save(@RequestBody Condition[] conditions) {
 		List<Condition> savedConditions = new ArrayList<Condition>();
 		for (Condition condition : conditions) {
-			org.openmrs.module.emrapi.conditionslist.Condition savedCondition = conditionService.save(conditionMapper.map(condition));
-			savedConditions.add(conditionMapper.map(savedCondition));
+			Condition savedCondition = conditionService.saveCondition(condition);
+			savedConditions.add(savedCondition);
 		}
 		return savedConditions;
 	}
 }
+
