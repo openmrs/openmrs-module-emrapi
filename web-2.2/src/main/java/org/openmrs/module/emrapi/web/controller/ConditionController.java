c367ec554b3daa538b6a08708e9a5056d61b0e0a
Arthur236 <arthur.thungu@gmail.com>
2018-05-29 23:12:40 +0300

    TRUNK-5373: Implement the new ConditionController.

--- web-2.2/src/main/java/org/openmrs/module/emrapi/web/controller/ConditionController.java
+++ web-2.2/src/main/java/org/openmrs/module/emrapi/web/controller/ConditionController.java
@@ -1,12 +1,28 @@
 package org.openmrs.module.emrapi.web.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+import static org.openmrs.util.LocaleUtility.getDefaultLocale;
 
-import org.openmrs.Condition;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openmrs.CodedOrFreeText;
+import org.openmrs.Concept;
+import org.openmrs.ConceptName;
+import org.openmrs.ConditionClinicalStatus;
+import org.openmrs.Patient;
 import org.openmrs.api.ConceptService;
 import org.openmrs.api.ConditionService;
 import org.openmrs.api.PatientService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.emrapi.conditionslist.Condition;
+import org.openmrs.module.emrapi.conditionslist.ConditionHistory;
+import org.openmrs.module.emrapi.conditionslist.ConditionListConstants;
+import org.openmrs.module.emrapi.conditionslist.contract.ConditionHistoryMapper;
+import org.openmrs.module.emrapi.conditionslist.contract.ConditionMapper;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,6 +38,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "/rest/emrapi")
 public class ConditionController extends BaseRestController {
+
+	ConditionMapper conditionMapper = new ConditionMapper();
+
+	ConditionHistoryMapper conditionHistoryMapper = new ConditionHistoryMapper(conditionMapper);
 
 	ConditionService conditionService;
 
@@ -45,6 +65,303 @@ public class ConditionController extends BaseRestController {
 	}
 
 	/**
+	 * Method to return a concept name
+	 *
+	 * @param names - a collection of concept name objects
+	 * @param name - name of a concept
+	 *
+	 * @return null or currentName
+	 */
+	private ConceptName getName(Collection<ConceptName> names, String name) {
+		if (name == null) {
+			return null;
+		}
+
+		for (ConceptName currentName : names) {
+			if (name.equalsIgnoreCase(currentName.getName())) {
+				return currentName;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Method to convert clinical status of core condition objects into status of condition objects of the emrapi module
+	 *
+	 * @param clinicalStatus - a ConditionClinicalStatus object
+	 *
+	 * @return convertedStatus
+	 */
+	private Condition.Status convertClinicalStatus(ConditionClinicalStatus clinicalStatus) {
+		Condition.Status convertedStatus = Condition.Status.ACTIVE;
+
+		if (clinicalStatus == ConditionClinicalStatus.ACTIVE) {
+			convertedStatus = Condition.Status.ACTIVE;
+		} else if (clinicalStatus == ConditionClinicalStatus.INACTIVE) {
+			convertedStatus = Condition.Status.INACTIVE;
+		} else if (clinicalStatus == ConditionClinicalStatus.HISTORY_OF) {
+			convertedStatus = Condition.Status.HISTORY_OF;
+		}
+
+		return convertedStatus;
+	}
+
+	/**
+	 * Method to convert Condition.Status object of the emrapi module into ConditionClinicalStatus object of the core module
+	 *
+	 * @param status - a Condition.Status object
+	 *
+	 * @return convertedStatus
+	 */
+	private ConditionClinicalStatus convertConditionListStatus(Condition.Status status) {
+		ConditionClinicalStatus convertedStatus = ConditionClinicalStatus.ACTIVE;
+
+		if (status == Condition.Status.ACTIVE) {
+			convertedStatus = ConditionClinicalStatus.ACTIVE;
+		} else if (status == Condition.Status.INACTIVE) {
+			convertedStatus = ConditionClinicalStatus.INACTIVE;
+		} else if (status == Condition.Status.HISTORY_OF) {
+			convertedStatus = ConditionClinicalStatus.HISTORY_OF;
+		}
+
+		return convertedStatus;
+	}
+
+	/**
+	 * Method to convert an org.openmrs.Concept object into an
+	 * org.openmrs.module.emrapi.conditionslist.contract.Concept object
+	 *
+	 * @param coreConcept - a Condition.Status object
+	 *
+	 * @return concept
+	 */
+	private org.openmrs.module.emrapi.conditionslist.contract.Concept convertCoreConceptToEmrapiConcept(Concept coreConcept) {
+		ConceptName fullySpecifiedName = coreConcept.getFullySpecifiedName(Context.getLocale());
+		if (fullySpecifiedName == null) {
+			fullySpecifiedName = coreConcept.getFullySpecifiedName(getDefaultLocale());
+		}
+		org.openmrs.module.emrapi.conditionslist.contract.Concept concept =
+				new org.openmrs.module.emrapi.conditionslist.contract.Concept(coreConcept.getUuid(),
+						fullySpecifiedName.getName());
+		ConceptName shortName = coreConcept.getShortNameInLocale(Context.getLocale());
+
+		if (shortName != null) {
+			concept.setShortName(shortName.getName());
+		}
+		return concept;
+	}
+
+	/**
+	 * Method to convert a list of org.openmrs.Condition objects into a list of
+	 * org.openmrs.module.emrapi.conditionslist.Condition objects
+	 *
+	 * @param coreConditions - a list of org.openmrs.Condition objects
+	 *
+	 * @return conditions
+	 */
+	private List<Condition> convertCoreConditionsToEmrapiConditions(List<org.openmrs.Condition> coreConditions) {
+		List<Condition> conditions = new ArrayList<Condition>();
+		for (org.openmrs.Condition coreCondition : coreConditions) {
+			Condition condition = new Condition();
+			Concept concept;
+
+			if (coreCondition.getCondition().getCoded() != null) {
+				concept = Context.getConceptService()
+						.getConceptByUuid(coreCondition.getCondition().getCoded().getUuid());
+			} else {
+				concept = new Concept();
+				concept.setFullySpecifiedName(new ConceptName(coreCondition.getCondition().getNonCoded(), Context.getLocale()));
+			}
+
+			condition.setId(coreCondition.getId());
+			condition.setUuid(coreCondition.getUuid());
+			condition.setAdditionalDetail(coreCondition.getAdditionalDetail());
+			condition.setConcept(concept);
+			condition.setPatient(coreCondition.getPatient());
+
+			if (coreCondition.getCondition().getNonCoded() != null) {
+				condition.setConditionNonCoded(coreCondition.getCondition().getNonCoded());
+			}
+
+			if (coreCondition.getOnsetDate() != null) {
+				condition.setOnsetDate(coreCondition.getOnsetDate());
+			}
+			condition.setVoided(coreCondition.getVoided());
+			condition.setVoidReason(coreCondition.getVoidReason());
+			condition.setEndDate(coreCondition.getEndDate());
+			condition.setCreator(coreCondition.getCreator());
+			condition.setDateCreated(coreCondition.getDateCreated());
+			condition.setStatus(convertClinicalStatus(coreCondition.getClinicalStatus()));
+			if (coreCondition.getPreviousVersion() != null) {
+				condition.setPreviousCondition(convertCoreConditionToEmrapiCondition(coreCondition.getPreviousVersion()));
+			}
+
+			conditions.add(condition);
+		}
+		return conditions;
+	}
+
+	/**
+	 * Method to convert a list of core active conditions into a list of condition history objects
+	 *
+	 * @param coreConditions - a list of org.openmrs.Condition objects
+	 *
+	 * @return conditionHistory
+	 */
+	private List<ConditionHistory> convertHistory(List<org.openmrs.Condition> coreConditions) {
+		List<Condition> convertedConditions = convertCoreConditionsToEmrapiConditions(coreConditions);
+		Map<String, ConditionHistory> allConditions = new LinkedHashMap<String, ConditionHistory>();
+
+		for (Condition condition : convertedConditions) {
+			Concept concept = condition.getConcept();
+
+			String nonCodedConceptUuid = Context.getAdministrationService().getGlobalProperty(
+					ConditionListConstants.GLOBAL_PROPERTY_NON_CODED_UUID);
+
+			String key = concept.getUuid().equals(nonCodedConceptUuid) ?
+					condition.getConditionNonCoded() :
+					concept.getUuid();
+			ConditionHistory conditionHistory = allConditions.get(key);
+
+			if (conditionHistory != null) {
+				conditionHistory.getConditions().add(condition);
+			} else {
+				conditionHistory = new ConditionHistory();
+				List<Condition> conditions = new ArrayList<Condition>();
+				conditions.add(condition);
+				conditionHistory.setConditions(conditions);
+				conditionHistory.setCondition(condition.getConcept());
+				if (concept.getUuid().equals(nonCodedConceptUuid)) {
+					conditionHistory.setNonCodedCondition(condition.getConditionNonCoded());
+				}
+			}
+			allConditions.put(key, conditionHistory);
+		}
+		return new ArrayList<ConditionHistory>(allConditions.values());
+	}
+
+	/**
+	 * Method to convert an org.openmrs.module.emrapi.conditionslist.contract.Condition object into an org.openmrs.Condition
+	 * object
+	 *
+	 * @param condition - an org.openmrs.module.emrapi.conditionslist.contract.Condition object
+	 *
+	 * @return cond
+	 */
+	private org.openmrs.Condition convertEmrapiContractConditionToCoreCondition(org.openmrs.module.emrapi.conditionslist.contract.Condition condition) {
+		Concept concept = Context.getConceptService().getConceptByUuid(condition.getConcept().getUuid());
+		Patient patient = Context.getPatientService().getPatientByUuid(condition.getPatientUuid());
+		String nonCodedConditionConcept = Context.getAdministrationService().getGlobalProperty(
+				ConditionListConstants.GLOBAL_PROPERTY_NON_CODED_UUID);
+
+		org.openmrs.Condition openmrsCondition = new org.openmrs.Condition();
+		CodedOrFreeText codedOrFreeText;
+
+		if (!isEmpty(condition.getConditionNonCoded())) {
+			concept = Context.getConceptService().getConceptByUuid(nonCodedConditionConcept);
+		}
+		if (condition.getUuid() != null) {
+			openmrsCondition.setUuid(condition.getUuid());
+		}
+
+		if (concept != null) {
+			ConceptName fullySpecifiedName = getName(concept.getNames(), condition.getConcept().getName());
+			codedOrFreeText = new CodedOrFreeText(concept, fullySpecifiedName, condition.getConditionNonCoded());
+		} else {
+			codedOrFreeText = new CodedOrFreeText(null, null, condition.getConditionNonCoded());
+		}
+
+		openmrsCondition.setAdditionalDetail(condition.getAdditionalDetail());
+		openmrsCondition.setClinicalStatus(convertConditionListStatus(condition.getStatus()));
+		openmrsCondition.setCondition(codedOrFreeText);
+		openmrsCondition.setPatient(patient);
+		openmrsCondition.setOnsetDate(condition.getOnSetDate());
+		openmrsCondition.setEndDate(condition.getEndDate());
+		openmrsCondition.setVoided(condition.getVoided());
+		openmrsCondition.setVoidReason(condition.getVoidReason());
+
+		return openmrsCondition;
+	}
+
+	/**
+	 * Method to convert an org.openmrs.Condition object into an org.openmrs.module.emrapi.conditionslist.contract.Condition
+	 * object
+	 *
+	 * @param coreCondition -a core condition object
+	 *
+	 * @return contractCondition
+	 */
+	private org.openmrs.module.emrapi.conditionslist.contract.Condition convertCoreConditionToEmrapiContractCondition(org.openmrs.Condition coreCondition) {
+		org.openmrs.module.emrapi.conditionslist.contract.Concept concept = new org.openmrs.module.emrapi.conditionslist.contract.Concept();
+		if (coreCondition.getCondition().getCoded() != null) {
+			concept = convertCoreConceptToEmrapiConcept(coreCondition.getCondition().getCoded());
+		}
+		org.openmrs.module.emrapi.conditionslist.contract.Condition contractCondition = new org.openmrs.module.emrapi.conditionslist.contract.Condition();
+		CodedOrFreeText codedOrFreeText = coreCondition.getCondition();
+
+		contractCondition.setUuid(coreCondition.getUuid());
+		contractCondition.setAdditionalDetail(coreCondition.getAdditionalDetail());
+		contractCondition.setStatus(convertClinicalStatus(coreCondition.getClinicalStatus()));
+		contractCondition.setConcept(concept);
+		contractCondition.setPatientUuid(coreCondition.getPatient().getUuid());
+		contractCondition.setConditionNonCoded(codedOrFreeText.getNonCoded());
+		contractCondition.setOnSetDate(coreCondition.getOnsetDate());
+		contractCondition.setVoided(coreCondition.getVoided());
+		contractCondition.setVoidReason(coreCondition.getVoidReason());
+		contractCondition.setEndDate(coreCondition.getEndDate());
+		contractCondition.setCreator(coreCondition.getCreator().getUuid());
+		contractCondition.setDateCreated(coreCondition.getDateCreated());
+		if (coreCondition.getPreviousVersion() != null) {
+			contractCondition.setPreviousConditionUuid(convertCoreConditionToEmrapiCondition(coreCondition.getPreviousVersion()).getUuid());
+		}
+
+		return contractCondition;
+	}
+
+	/**
+	 * Method to convert an org.openmrs.Condition object into an org.openmrs.module.emrapi.conditionslist.Condition
+	 * object
+	 *
+	 * @param coreCondition - an org.openmrs.Condition object
+	 *
+	 * @return cListCondition
+	 */
+	private org.openmrs.module.emrapi.conditionslist.Condition convertCoreConditionToEmrapiCondition(org.openmrs.Condition coreCondition) {
+		org.openmrs.module.emrapi.conditionslist.Condition cListCondition = new org.openmrs.module.emrapi.conditionslist.Condition();
+		Concept concept;
+
+		if (coreCondition.getCondition().getCoded() != null) {
+			concept = Context.getConceptService()
+					.getConceptByUuid(coreCondition.getCondition().getCoded().getUuid());
+
+			if(coreCondition.getCondition().getSpecificName() == null) {
+				coreCondition.getCondition().setSpecificName(coreCondition.getCondition().getCoded().getName(Context.getLocale()));
+			}
+		} else {
+			concept = new Concept();
+		}
+
+		cListCondition.setUuid(coreCondition.getUuid());
+		cListCondition.setConcept(concept);
+		cListCondition.setAdditionalDetail(coreCondition.getAdditionalDetail());
+		cListCondition.setPatient(coreCondition.getPatient());
+		cListCondition.setConditionNonCoded(coreCondition.getCondition().getNonCoded());
+		cListCondition.setOnsetDate(coreCondition.getOnsetDate());
+		cListCondition.setVoided(coreCondition.getVoided());
+		cListCondition.setVoidReason(coreCondition.getVoidReason());
+		cListCondition.setEndDate(coreCondition.getEndDate());
+		cListCondition.setCreator(coreCondition.getCreator());
+		cListCondition.setDateCreated(coreCondition.getDateCreated());
+		cListCondition.setStatus(convertClinicalStatus(coreCondition.getClinicalStatus()));
+		if (coreCondition.getPreviousVersion() != null) {
+			cListCondition.setPreviousCondition(convertCoreConditionToEmrapiCondition(coreCondition.getPreviousVersion()));
+		}
+
+		return cListCondition;
+	}
+
+	/**
 	 * Gets a list of active conditions.
 	 *
 	 * @param patientUuid - the uuid of a patient
@@ -52,9 +369,10 @@ public class ConditionController extends BaseRestController {
 	 */
 	@RequestMapping(method = RequestMethod.GET, value = "/conditionhistory")
 	@ResponseBody
-	public List<Condition> getConditionHistory(@RequestParam("patientUuid") String patientUuid) {
+	public List<org.openmrs.module.emrapi.conditionslist.contract.ConditionHistory> getConditionHistory(@RequestParam("patientUuid") String patientUuid) {
+		List<org.openmrs.Condition> activeConditions = conditionService.getActiveConditions(patientService.getPatientByUuid(patientUuid));
 
-		return conditionService.getActiveConditions(patientService.getPatientByUuid(patientUuid));
+		return conditionHistoryMapper.map(convertHistory(activeConditions));
 	}
 
 	/**
@@ -64,13 +382,17 @@ public class ConditionController extends BaseRestController {
 	 */
 	@RequestMapping(method = RequestMethod.POST, value = "/condition")
 	@ResponseBody
-	public List<Condition> save(@RequestBody Condition[] conditions) {
-		List<Condition> savedConditions = new ArrayList<Condition>();
-		for (Condition condition : conditions) {
-			Condition savedCondition = conditionService.saveCondition(condition);
-			savedConditions.add(savedCondition);
+	public List<org.openmrs.module.emrapi.conditionslist.contract.Condition> save(@RequestBody org.openmrs.module.emrapi.conditionslist.contract.Condition[] conditions) {
+
+		List<org.openmrs.module.emrapi.conditionslist.contract.Condition> savedConditions = new ArrayList<org.openmrs.module.emrapi.conditionslist.contract.Condition>();
+
+		for (org.openmrs.module.emrapi.conditionslist.contract.Condition condition : conditions) {
+			org.openmrs.Condition savedCondition = conditionService.saveCondition(
+					convertEmrapiContractConditionToCoreCondition(condition));
+			savedConditions.add(convertCoreConditionToEmrapiContractCondition(savedCondition));
 		}
 		return savedConditions;
 	}
+
 }
 
