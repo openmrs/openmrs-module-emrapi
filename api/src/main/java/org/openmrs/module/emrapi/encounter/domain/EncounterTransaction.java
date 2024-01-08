aa45884a2a68176a54bb571b189926db6c4ccd66
riyaTw <126869458+riyaTw@users.noreply.github.com>
2024-01-08 15:38:15 +0530

    BAH-3448 | Add. Accession number field in Order class (#219)
    * BAH-3448 | Add. Accession number field in Order class | [Riya/Sweety]
    
    * BAH-3448 | Add. Since annotation in public methods of accessionNumber | [Riya/Sweety]

--- api/src/main/java/org/openmrs/module/emrapi/encounter/domain/EncounterTransaction.java
+++ api/src/main/java/org/openmrs/module/emrapi/encounter/domain/EncounterTransaction.java
@@ -632,6 +632,7 @@ public class EncounterTransaction {
         private String instructions;
         private String uuid;
         private String orderType;
+        private String accessionNumber;
         private OrderGroup orderGroup;
         private Date dateCreated;
         private Date dateChanged;
@@ -653,6 +654,20 @@ public class EncounterTransaction {
 
         public Date getAutoExpireDate() {
             return autoExpireDate;
+        }
+
+        /**
+         * @since 1.35.0
+         */
+        public String getAccessionNumber() {
+            return accessionNumber;
+        }
+
+        /**
+         * @since 1.35.0
+         */
+        public void setAccessionNumber(String accessionNumber) {
+            this.accessionNumber = accessionNumber;
         }
 
         public void setAutoExpireDate(Date autoExpireDate) {
