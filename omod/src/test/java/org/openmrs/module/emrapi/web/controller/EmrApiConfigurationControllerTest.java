47b2aa17072ec9c1bd9c636ff335cefeac5ffba4
Michael Seaton <mseaton@pih.org>
2024-06-27 08:34:58 -0400

    Generalize

--- omod/src/test/java/org/openmrs/module/emrapi/web/controller/EmrApiConfigurationControllerTest.java
+++ omod/src/test/java/org/openmrs/module/emrapi/web/controller/EmrApiConfigurationControllerTest.java
@@ -1,21 +1,27 @@
 package org.openmrs.module.emrapi.web.controller;
 
 import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.module.emrapi.EmrApiProperties;
 import org.openmrs.module.metadatamapping.api.MetadataMappingService;
 import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.web.representation.Representation;
+import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
+import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs2_0.LocationResource2_0;
 import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import java.util.Map;
+import java.util.Set;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class EmrApiConfigurationControllerTest extends BaseModuleWebContextSensitiveTest {
 
@@ -40,17 +46,43 @@ public class EmrApiConfigurationControllerTest extends BaseModuleWebContextSensi
 	}
 
 	@Test
-	public void shouldGetDefaultRepresentation() throws Exception {
+	public void shouldGetAsJson() throws Exception {
+		request.addParameter("v", "full");
 		SimpleObject config = emrApiConfigurationController.getEmrApiConfiguration(request, response);
-		System.out.println(new ObjectMapper().writeValueAsString(config));
-		assertEquals(46, config.keySet().size());
-		assertEquals("org.openmrs.module.emrapi", config.get("metadataSourceName"));
-		assertEquals("50", config.get("lastViewedPatientSizeLimit").toString());
-		assertEquals("Unknown Location", mapNode(config, "unknownLocation").get("display"));
+		String jsonString = new ObjectMapper().writeValueAsString(config);
+		Assert.assertTrue(jsonString.contains("unknownLocation"));
 	}
 
 	@Test
-	public void shouldGetCustomRepresentation() throws Exception {
+	public void shouldGetDefaultRepresentation() {
+		SimpleObject config = emrApiConfigurationController.getEmrApiConfiguration(request, response);
+		assertEquals(46, config.keySet().size());
+		assertEquals("org.openmrs.module.emrapi", config.get("metadataSourceName"));
+		assertEquals("50", config.get("lastViewedPatientSizeLimit").toString());
+		Map<String, Object> unknownLocation = mapNode(config, "unknownLocation");
+		assertEquals(3, unknownLocation.size());
+		assertThat(unknownLocation.keySet(), containsInAnyOrder("uuid", "display", "links"));
+		assertEquals("Unknown Location", unknownLocation.get("display"));
+	}
+
+	@Test
+	public void shouldGetFullRepresentation() {
+		request.addParameter("v", "full");
+		SimpleObject config = emrApiConfigurationController.getEmrApiConfiguration(request, response);
+		Map<String, Object> unknownLocation = mapNode(config, "unknownLocation");
+		DelegatingResourceDescription drd = new LocationResource2_0().getRepresentationDescription(Representation.FULL);
+		Set<String> expectedProps = drd.getProperties().keySet();
+		for (String prop : expectedProps) {
+			Assert.assertTrue("Expected property: " + prop, unknownLocation.containsKey(prop));
+		}
+		for (int i=1; i<=15; i++) {
+			assertTrue(unknownLocation.containsKey("address"+i));
+		}
+		assertEquals("Unknown Location", unknownLocation.get("display"));
+	}
+
+	@Test
+	public void shouldGetCustomRepresentation() {
 		request.addParameter("v", "custom:(unknownLocation:(display),admissionEncounterType:full)");
 		SimpleObject config = emrApiConfigurationController.getEmrApiConfiguration(request, response);
 		assertEquals(2, config.size());
