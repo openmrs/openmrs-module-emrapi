e2d012396fd6ef3a92d17a0a0cd5b34e008ee33f
Michael Seaton <mseaton@pih.org>
2024-07-12 10:45:06 -0400

    Change default representation of inpatient request to ref for all properties except visit, which is default

--- omod/src/main/java/org/openmrs/module/emrapi/rest/converter/InpatientRequestConverter.java
+++ omod/src/main/java/org/openmrs/module/emrapi/rest/converter/InpatientRequestConverter.java
@@ -5,6 +5,7 @@ import org.apache.commons.logging.LogFactory;
 import org.openmrs.annotation.Handler;
 import org.openmrs.module.emrapi.adt.InpatientRequest;
 import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.FullRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
 
@@ -13,10 +14,6 @@ public class InpatientRequestConverter extends SimpleBeanConverter<InpatientRequ
 
     private final Log log = LogFactory.getLog(getClass());
 
-    /**
-     * @return a representation of each property that matches the given representation, except when
-     * requesting the default representation, in which case all properties are ref except for visit which is default
-     */
     @Override
     public DelegatingResourceDescription getResourceDescription(InpatientRequest req, Representation representation) {
         DelegatingResourceDescription ret = super.getResourceDescription(req, representation);
@@ -24,6 +21,13 @@ public class InpatientRequestConverter extends SimpleBeanConverter<InpatientRequ
             for (String property : ret.getProperties().keySet()) {
                 if (!property.equals("visit")) {
                     ret.addProperty(property, Representation.REF);
+                }
+            }
+        }
+        else if (representation instanceof FullRepresentation) {
+            for (String property : ret.getProperties().keySet()) {
+                if (!property.equals("visit")) {
+                    ret.addProperty(property, Representation.DEFAULT);
                 }
             }
         }
