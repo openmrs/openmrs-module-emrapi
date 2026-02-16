92f1e30e4036b1b1b272127273f7c8a0843031c5
Jayasank Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2026-02-16 18:18:12 +0530

    Convert from controller to resource

---
+++ omod/src/main/java/org/openmrs/module/emrapi/rest/resource/ProcedureResource.java
@@ -0,0 +1,125 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.emrapi.rest.resource;
+
+import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.emrapi.procedure.Procedure;
+import org.openmrs.module.emrapi.procedure.ProcedureService;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.FullRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.Representation;
+import org.openmrs.module.webservices.rest.web.resource.api.PageableResult;
+import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource;
+import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
+import org.openmrs.module.webservices.rest.web.resource.impl.NeedsPaging;
+import org.openmrs.module.webservices.rest.web.response.ResourceDoesNotSupportOperationException;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
+
+@Resource(name = RestConstants.VERSION_1 + "/procedure", supportedClass = Procedure.class, supportedOpenmrsVersions = { "2.2 - 9.*" })
+public class ProcedureResource extends DelegatingCrudResource<Procedure> {
+
+	@Override
+	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
+		if (rep instanceof DefaultRepresentation || rep instanceof FullRepresentation) {
+			DelegatingResourceDescription description = new DelegatingResourceDescription();
+			description.addProperty("uuid");
+			description.addProperty("patient", Representation.REF);
+			description.addProperty("encounter", Representation.REF);
+			description.addProperty("procedureCoded", Representation.REF);
+			description.addProperty("procedureNonCoded");
+			description.addProperty("bodySite", Representation.REF);
+			description.addProperty("startDateTime");
+			description.addProperty("estimatedStartDate");
+			description.addProperty("endDateTime");
+			description.addProperty("duration");
+			description.addProperty("durationUnit", Representation.REF);
+			description.addProperty("status", Representation.REF);
+			description.addProperty("outcomeCoded", Representation.REF);
+			description.addProperty("outcomeNonCoded");
+			description.addProperty("notes");
+			description.addProperty("formNamespace");
+			description.addProperty("formFieldPath");
+			description.addProperty("voided");
+			description.addSelfLink();
+			return description;
+		}
+		return null;
+	}
+
+	@Override
+	public DelegatingResourceDescription getCreatableProperties() throws ResourceDoesNotSupportOperationException {
+		DelegatingResourceDescription description = new DelegatingResourceDescription();
+		description.addProperty("patient");
+		description.addProperty("encounter");
+		description.addProperty("procedureCoded");
+		description.addProperty("procedureNonCoded");
+		description.addProperty("bodySite");
+		description.addProperty("startDateTime");
+		description.addProperty("estimatedStartDate");
+		description.addProperty("endDateTime");
+		description.addProperty("duration");
+		description.addProperty("durationUnit");
+		description.addProperty("status");
+		description.addProperty("outcomeCoded");
+		description.addProperty("outcomeNonCoded");
+		description.addProperty("notes");
+		description.addProperty("formNamespace");
+		description.addProperty("formFieldPath");
+		return description;
+	}
+
+	@Override
+	public DelegatingResourceDescription getUpdatableProperties() throws ResourceDoesNotSupportOperationException {
+		return getCreatableProperties();
+	}
+
+	@Override
+	public Procedure newDelegate() {
+		return new Procedure();
+	}
+
+	@Override
+	public Procedure save(Procedure procedure) {
+		return Context.getService(ProcedureService.class).saveProcedure(procedure);
+	}
+
+	@Override
+	public Procedure getByUniqueId(String uuid) {
+		return Context.getService(ProcedureService.class).getProcedureByUuid(uuid);
+	}
+
+	@Override
+	protected PageableResult doGetAll(RequestContext context) throws ResponseException {
+		String patientUuid = context.getParameter("patient");
+		if (patientUuid != null) {
+			Patient patient = Context.getPatientService().getPatientByUuid(patientUuid);
+			if (patient == null) {
+				throw new IllegalArgumentException("Patient not found with UUID: " + patientUuid);
+			}
+			return new NeedsPaging<>(
+					Context.getService(ProcedureService.class).getProceduresByPatient(patient), context);
+		}
+		throw new ResourceDoesNotSupportOperationException("A patient parameter is required");
+	}
+
+	@Override
+	protected void delete(Procedure procedure, String reason, RequestContext context) throws ResponseException {
+		throw new ResourceDoesNotSupportOperationException();
+	}
+
+	@Override
+	public void purge(Procedure procedure, RequestContext context) throws ResponseException {
+		throw new ResourceDoesNotSupportOperationException();
+	}
+}
---
+++ omod/src/test/java/org/openmrs/module/emrapi/rest/resource/ProcedureResourceTest.java
@@ -0,0 +1,186 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.emrapi.rest.resource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.openmrs.module.emrapi.procedure.Procedure;
+import org.openmrs.module.emrapi.procedure.ProcedureService;
+import org.openmrs.web.test.jupiter.BaseModuleWebContextSensitiveTest;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class ProcedureResourceTest extends BaseModuleWebContextSensitiveTest {
+   
+   private static final String TEST_DATASET = "ProcedureControllerTestDataset.xml";
+   
+   private static final String PATIENT_UUID = "procedure-test-patient-uuid";
+   private static final String PROCEDURE_CONCEPT_UUID = "procedure-concept-uuid";
+   private static final String BODY_SITE_UUID = "body-site-concept-uuid";
+   private static final String STATUS_UUID = "status-completed-concept-uuid";
+   private static final String EXISTING_PROCEDURE_UUID = "existing-procedure-uuid";
+   
+   private static final String BASE_URL = "/rest/v1/emrapi/procedure";
+   
+   @Autowired
+   private ObjectFactory<ProcedureResource> controllerFactory;
+   
+   @Autowired
+   private ProcedureService procedureService;
+   
+   private MockMvc mockMvc;
+   
+   private ObjectMapper objectMapper;
+   
+   @BeforeEach
+   void setUp() {
+      executeDataSet(TEST_DATASET);
+      mockMvc = MockMvcBuilders.standaloneSetup(controllerFactory.getObject()).build();
+      objectMapper = new ObjectMapper();
+   }
+   
+   private Map<String, Object> parseResponse(MvcResult result) throws Exception {
+      return objectMapper.readValue(result.getResponse().getContentAsString(), Map.class);
+   }
+   
+   private List<Map<String, Object>> parseListResponse(MvcResult result) throws Exception {
+      return objectMapper.readValue(result.getResponse().getContentAsString(), List.class);
+   }
+   
+   @Nested
+   @DisplayName("GET /rest/v1/emrapi/procedure?patient={uuid}")
+   class GetProceduresByPatient {
+      
+      @Test
+      void shouldReturnProceduresForPatient() throws Exception {
+         MvcResult result = mockMvc.perform(get(BASE_URL).param("patient", PATIENT_UUID))
+                 .andExpect(status().isOk())
+                 .andReturn();
+         
+         List<Map<String, Object>> response = parseListResponse(result);
+         assertEquals(1, response.size());
+         assertEquals(EXISTING_PROCEDURE_UUID, response.get(0).get("uuid"));
+         assertEquals(PATIENT_UUID, response.get(0).get("patientUuid"));
+         assertEquals("Existing procedure for GET test", response.get(0).get("notes"));
+      }
+   }
+   
+   @Nested
+   @DisplayName("GET /rest/v1/emrapi/procedure/{uuid}")
+   class GetProcedureByUuid {
+      
+      @Test
+      void shouldReturnProcedureByUuid() throws Exception {
+         MvcResult result = mockMvc.perform(get(BASE_URL + "/" + EXISTING_PROCEDURE_UUID))
+                 .andExpect(status().isOk())
+                 .andReturn();
+         
+         Map<String, Object> response = parseResponse(result);
+         assertEquals(EXISTING_PROCEDURE_UUID, response.get("uuid"));
+         assertEquals(PATIENT_UUID, response.get("patientUuid"));
+         assertEquals(PROCEDURE_CONCEPT_UUID, response.get("codedProcedureUuid"));
+         assertEquals(BODY_SITE_UUID, response.get("bodySiteUuid"));
+         assertEquals(STATUS_UUID, response.get("statusUuid"));
+         assertEquals(45, response.get("duration"));
+         assertEquals("MINUTES", response.get("durationUnit"));
+         assertEquals("Existing procedure for GET test", response.get("notes"));
+      }
+   }
+   
+   @Nested
+   @DisplayName("POST /rest/v1/emrapi/procedure/current")
+   class CreateCurrentProcedure {
+      
+      @Test
+      void shouldCreateCurrentProcedure() throws Exception {
+         String requestBody = "{"
+                 + "\"patientUuid\": \"" + PATIENT_UUID + "\","
+                 + "\"codedProcedureUuid\": \"" + PROCEDURE_CONCEPT_UUID + "\","
+                 + "\"bodySiteUuid\": \"" + BODY_SITE_UUID + "\","
+                 + "\"statusUuid\": \"" + STATUS_UUID + "\","
+                 + "\"startDateTime\": \"2025-06-15T14:30:00\","
+                 + "\"duration\": 45,"
+                 + "\"durationUnit\": \"MINUTES\","
+                 + "\"notes\": \"Current procedure notes\""
+                 + "}";
+         
+         MvcResult result = mockMvc.perform(post(BASE_URL + "/current")
+                         .contentType(MediaType.APPLICATION_JSON)
+                         .content(requestBody))
+                 .andExpect(status().isCreated())
+                 .andReturn();
+         
+         Map<String, Object> response = parseResponse(result);
+         assertNotNull(response.get("uuid"));
+         assertEquals(PATIENT_UUID, response.get("patientUuid"));
+         assertEquals(PROCEDURE_CONCEPT_UUID, response.get("codedProcedureUuid"));
+         assertEquals(BODY_SITE_UUID, response.get("bodySiteUuid"));
+         assertEquals(45, response.get("duration"));
+         assertEquals("MINUTES", response.get("durationUnit"));
+         assertEquals("Current procedure notes", response.get("notes"));
+         
+         // Verify persisted to DB
+         String uuid = (String) response.get("uuid");
+         Procedure saved = procedureService.getProcedureByUuid(uuid);
+         assertNotNull(saved);
+         assertEquals("Current procedure notes", saved.getNotes());
+      }
+   }
+   
+   @Nested
+   @DisplayName("POST /rest/v1/emrapi/procedure/historical")
+   class CreateHistoricalProcedure {
+      
+      @Test
+      void shouldCreateHistoricalProcedure() throws Exception {
+         String requestBody = "{"
+                 + "\"patientUuid\": \"" + PATIENT_UUID + "\","
+                 + "\"codedProcedureUuid\": \"" + PROCEDURE_CONCEPT_UUID + "\","
+                 + "\"bodySiteUuid\": \"" + BODY_SITE_UUID + "\","
+                 + "\"statusUuid\": \"" + STATUS_UUID + "\","
+                 + "\"estimatedStartDate\": \"2020-06\","
+                 + "\"notes\": \"Historical procedure notes\""
+                 + "}";
+         
+         MvcResult result = mockMvc.perform(post(BASE_URL + "/historical")
+                         .contentType(MediaType.APPLICATION_JSON)
+                         .content(requestBody))
+                 .andExpect(status().isCreated())
+                 .andReturn();
+         
+         Map<String, Object> response = parseResponse(result);
+         assertNotNull(response.get("uuid"));
+         assertEquals("2020-06", response.get("estimatedStartDate"));
+         assertEquals("Historical procedure notes", response.get("notes"));
+         
+         // Verify persisted to DB
+         String uuid = (String) response.get("uuid");
+         Procedure saved = procedureService.getProcedureByUuid(uuid);
+         assertNotNull(saved);
+         assertEquals("2020-06", saved.getEstimatedStartDate());
+      }
+   }
+}
