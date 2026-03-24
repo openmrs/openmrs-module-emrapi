28f102e13600596c53a14cd892fa8ed0a59d5393
Jayasank Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2026-03-24 13:26:22 +0530

    Remove wildcards

--- api/src/test/java/org/openmrs/module/emrapi/procedure/ProcedureDAOTest.java
+++ api/src/test/java/org/openmrs/module/emrapi/procedure/ProcedureDAOTest.java
@@ -18,7 +18,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Tests for {@link HibernateProcedureDAO}.
